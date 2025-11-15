bd2a8ffb0773281e404ac2a69fe18f5677083ed7
Sean Curtis <sean@Seans-MacBook-Air.local>
2025-11-15 14:51:32 -0500

    initial spring boot project

---
+++ src/main/java/com/curtis/todo/TodoApiJavaMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.curtis.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApiJavaMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoApiJavaMysqlApplication.class, args);
+	}
+
+}
